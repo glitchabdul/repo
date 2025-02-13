28875be61a9ba9e204889779b8fe6cbf17abfabf
glitchabdul <sampayots@gmail.com>
2025-02-13 15:06:29 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+import { MessageComp, MessageComps, StateCounter, Toggle } from './components/messagecomps';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MessageComp>Hi hello</MessageComp>
+      <MessageComps name="Ram" age="20" ></MessageComps>
+      <StateCounter></StateCounter>
+      <Toggle></Toggle>
+      <br></br><Button variant="text">Text</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/messagecomps.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+
+export function MessageComp(){
+    return(
+      <div id="container">
+        <h1>Hello how are you </h1>
+        </div>
+    );
+}
+
+
+export function MessageComps(props){
+    return(
+        <div id="container">
+            <h1>{props.name} {props.age}</h1>
+        </div>
+    );
+   
+}
+
+export function StateCounter() {
+    const [count, setCount] = useState(0); 
+    function clickHandler(){
+        alert("Clicked");
+        setCount(count + 1);
+      }
+    return (
+        <div>
+          <p>You clicked {count} times</p>
+          <button onClick = {clickHandler}>Click me</button>
+        </div>
+      );
+    }
+
+    export function Toggle(){
+      const [isOn, setIsOn] =useState(false);
+      return(
+        <button onClick= { () => setIsOn(!isOn)}>
+          {isOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
